3de423f56530502d0f8107186ef2e07da2f24563
amorim-leo <57546427+amorim-leo@users.noreply.github.com>
2019-12-02 11:42:47 -0300

    Primeiros programas criados em C.

---
+++ listaAExe3_ImprimiProduto.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int n1;
+    int n2;
+    int n3;
+    
+    n1=30;
+    n2=27;
+    
+    n3=30*27;
+    
+    printf("O produto dos números 30 e 27 é: %d", n3);
+    
+    system("PAUSE");
+    return 0;
+}
